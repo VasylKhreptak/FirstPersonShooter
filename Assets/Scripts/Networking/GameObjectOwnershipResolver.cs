9d8c9e3a3cbb586a00c9db8db4e4997bc1faaac8
DESKTOP-5QT8APO\Vasyl Khreptak <VasylKhreptak12@gmail.com>
2024-04-10 20:01:07 +0300

    - fixed client camera frame show on server;

--- Assets/Scripts/Networking/GameObjectOwnershipResolver.cs
+++ Assets/Scripts/Networking/GameObjectOwnershipResolver.cs
@@ -1,14 +1,21 @@
 using FishNet.Object;
+using UnityEngine;
 
 namespace Networking
 {
     public class GameObjectOwnershipResolver : NetworkBehaviour
     {
+        [Header("References")]
+        [SerializeField] private GameObject _gameObject;
+
+        [Header("Preferences")]
+        [SerializeField] private bool _inversed;
+
         public override void OnStartClient()
         {
             base.OnStartClient();
 
-            gameObject.SetActive(IsOwner);
+            _gameObject.SetActive(_inversed ? IsOwner : !IsOwner);
         }
     }
 }
